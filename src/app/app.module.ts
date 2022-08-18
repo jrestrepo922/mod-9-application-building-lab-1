4b8192f2cfb02e11c5590e8b07550830a8051256
Juan Martin Restrepo <tinto922@gmail.com>
2022-08-18 08:48:23 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationComponentComponent } from './application-component/application-component.component';
+import { ConversationHistoryComponentComponent } from './conversation-history-component/conversation-history-component.component';
+import { ConversationThreadComponentComponent } from './conversation-thread-component/conversation-thread-component.component';
+import { ContactListComponentComponent } from './contact-list-component/contact-list-component.component';
+import { ConversationControlComponentComponent } from './conversation-control-component/conversation-control-component.component';
+import { SendMessageComponentComponent } from './send-message-component/send-message-component.component';
+import { UserMessageComponentComponent } from './user-message-component/user-message-component.component';
+import { SenderMessageComponentComponent } from './sender-message-component/sender-message-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationComponentComponent,
+    ConversationHistoryComponentComponent,
+    ConversationThreadComponentComponent,
+    ContactListComponentComponent,
+    ConversationControlComponentComponent,
+    SendMessageComponentComponent,
+    UserMessageComponentComponent,
+    SenderMessageComponentComponent,
+    ContactComponentComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule,
